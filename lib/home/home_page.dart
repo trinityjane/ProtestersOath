2e37b574d16ec391b88fd99a8579f704dbb30949
trinityjane <neuromancer3@outlook.com>
2020-10-12 19:09:45 -0700

    adjustment of action icons in the app bar of the home page.

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -13,41 +13,55 @@ class HomePage extends StatelessWidget {
   //   return MaterialPageRoute<void>(builder: (_) => HomePage());
   // }
   final drawer = PrefService.getString('drawer', ignoreCache: true);
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AppDrawerBloc, AppDrawerState>(
         builder: (BuildContext context, AppDrawerState state) {
       return Scaffold(
-          drawer: this.drawer!='none' ? AppDrawer() : null,
-          appBar: AppBar(backgroundColor: Colors.grey,
-            title: Text('HOME'.tr(),
+          drawer: this.drawer != 'none' ? AppDrawer() : null,
+          appBar: AppBar(
+            backgroundColor: Colors.grey,
+            title: Text(
+              'HOME'.tr(),
               style: TextStyle(color: Colors.white),
             ),
             actions: [
-              IconButton(
-                icon: Icon(Icons.announcement, size: 25),
-                onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
-                    .add(ProtestPageEvent()),
-                tooltip: 'PROTESTS_TOOLTIP'.tr(),
-              ),
-              IconButton(
-                icon: Icon(Icons.art_track, size: 35),
-                onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
-                    .add(StoryPageEvent()),
-                tooltip: 'STORIES_TOOLTIP'.tr(),
-              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 3),
+                  child: IconButton(
+                    icon: Icon(Icons.announcement, size: 25),
+                    onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
+                        .add(ProtestPageEvent()),
+                    tooltip: 'PROTESTS_TOOLTIP'.tr(),
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(top: 0),
+                  child: IconButton(
+                    icon: Icon(Icons.art_track, size: 35),
+                    onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
+                        .add(StoryPageEvent()),
+                    tooltip: 'STORIES_TOOLTIP'.tr(),
+                  )),
             ],
           ),
           body: Stack(children: <Widget>[
             CustomPaint(
               size: Size.infinite,
-              child: Container(height: MediaQuery.of(context).size.height,),
-              painter: ShapesPainter((state as HomePageState).token.phoneNumber),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+              ),
+              painter:
+                  ShapesPainter((state as HomePageState).token.phoneNumber),
             ),
             Container(
                 alignment: Alignment(0.9, 0.91),
                 child: IconButton(
-                  icon: Icon(Icons.list, color: Colors.black, size: 30,),
+                  icon: Icon(
+                    Icons.list,
+                    color: Colors.black,
+                    size: 30,
+                  ),
                   tooltip: 'OATH_TOOLTIP'.tr(),
                   onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
                       .add(OathPageEvent()),
@@ -55,36 +69,57 @@ class HomePage extends StatelessWidget {
             Container(
                 alignment: Alignment(-.9, 0.91),
                 child: IconButton(
-                  icon: Icon(Icons.privacy_tip, color: Colors.black, size: 30,),
+                  icon: Icon(
+                    Icons.privacy_tip,
+                    color: Colors.black,
+                    size: 30,
+                  ),
                   tooltip: 'PRIVACY'.tr(),
                   onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
                       .add(PrivacyPageEvent()),
                 )),
-            drawer == "none" ? Container(
-                alignment: Alignment(-0.45, 0.91),
-                child: IconButton(
-                  icon: Icon(Icons.open_in_full, color: Colors.black, size: 30,),
-                  tooltip: 'VERIFY'.tr(),
-                  onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
-                      .add(VerifyPageEvent()),
-                )):Container(),
-            drawer == "none" ? Container(
-                alignment: Alignment(0.0, 0.91),
-                child: IconButton(
-                  icon: Icon(Icons.settings, color: Colors.black, size: 30,),
-                  tooltip: 'SETTINGS'.tr(),
-                  onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
-                      .add(SettingsPageEvent()),
-                )):Container(),
-            drawer == "none" ? Container(
-                alignment: Alignment(0.45, 0.91),
-                child: IconButton(
-                  icon: Icon(Icons.group,color: Colors.black,size: 30,),
-                  tooltip: 'ABOUT'.tr(),
-                  onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
-                      .add(AboutPageEvent()),
-                )):Container(),
-
+            drawer == "none"
+                ? Container(
+                    alignment: Alignment(-0.45, 0.91),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.open_in_full,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      tooltip: 'VERIFY'.tr(),
+                      onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
+                          .add(VerifyPageEvent()),
+                    ))
+                : Container(),
+            drawer == "none"
+                ? Container(
+                    alignment: Alignment(0.0, 0.91),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      tooltip: 'SETTINGS'.tr(),
+                      onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
+                          .add(SettingsPageEvent()),
+                    ))
+                : Container(),
+            drawer == "none"
+                ? Container(
+                    alignment: Alignment(0.45, 0.91),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.group,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                      tooltip: 'ABOUT'.tr(),
+                      onPressed: () => BlocProvider.of<AppDrawerBloc>(context)
+                          .add(AboutPageEvent()),
+                    ))
+                : Container(),
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
@@ -95,7 +130,6 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-
           ]));
     });
   }
